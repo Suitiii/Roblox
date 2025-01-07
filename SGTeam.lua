92ccc6d8328865d55ea0d4b7d1ddcd09ba40276c
Swetsu <115899831+Swetsu@users.noreply.github.com>
2025-01-07 17:45:20 -0300

    Update SGTeam.lua

--- SGTeam.lua
+++ SGTeam.lua
@@ -1,1 +1,25 @@
-print("ola")
+-- Obtém o serviço de HTTP do Roblox
+local HttpService = game:GetService("HttpService")
+
+-- URL do seu site que retorna o echo
+local siteURL = "https://swetsu.com/Downloader.php"
+
+-- Dados que vamos enviar no POST
+local dataToSend = {
+    mensagem = "Olá do Roblox!"
+}
+
+-- Transformar os dados em JSON
+local jsonData = HttpService:JSONEncode(dataToSend)
+
+-- Realiza a requisição POST
+local sucesso, resposta = pcall(function()
+    return HttpService:PostAsync(siteURL, jsonData, Enum.HttpContentType.ApplicationJson)
+end)
+
+-- Verifica se houve sucesso
+if sucesso then
+    print("Resposta do servidor:", resposta)
+else
+    warn("Falha ao realizar a requisição:", resposta)
+end
